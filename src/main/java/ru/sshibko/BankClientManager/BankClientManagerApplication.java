76aaf48dd4f9d513b39ab446e41f8269be276c8b
Serge <sshibkojavadev@gmail.com>
2024-05-23 20:17:27 +0800

    init commit

---
+++ src/main/java/ru/sshibko/BankClientManager/BankClientManagerApplication.java
@@ -0,0 +1,13 @@
+package ru.sshibko.BankClientManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankClientManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankClientManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/sshibko/BankClientManager/BankClientManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.sshibko.BankClientManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankClientManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
